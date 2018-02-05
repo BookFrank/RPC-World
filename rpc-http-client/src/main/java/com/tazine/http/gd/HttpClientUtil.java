aa5c47310dfa2c9e2c273e254a5d66bd65c10d7d
Frank <bookfrank@foxmail.com>
2018-02-05 17:51:45 +0800

    style (rpc-http-client) : format the code and comments.

---
+++ rpc-http-client/src/main/java/com/tazine/http/gd/HttpClientUtil.java
@@ -0,0 +1,17 @@
+package com.tazine.http.gd;
+
+import java.util.Map;
+
+/**
+ * HttpClientUtil
+ *
+ * @author frank
+ * @since 1.0.0
+ */
+public class HttpClientUtil {
+
+
+
+
+
+}
---
+++ rpc-http-client/src/main/java/com/tazine/http/gd/HttpsClientUtil.java
@@ -0,0 +1,10 @@
+package com.tazine.http.gd;
+
+/**
+ * HttpsClientUtil
+ *
+ * @author frank
+ * @since 1.0.0
+ */
+public class HttpsClientUtil {
+}
